1103d15b28fd3aa725a632c939b0438503ae0aec
pmarino84 <pietro.marino_84@hotmail.it>
2024-10-26 11:04:56 +0200

    feat: remove export defaults from submodules into mod.ts

--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
-export * from "./combineActions.ts";
+export { combineActions } from "./combineActions.ts";
 export * from "./constants.ts";
-export * from "./createAction.ts";
-export * from "./createDispatcher.ts";
-export * from "./splitActionTypes.ts";
+export { createAction } from "./createAction.ts";
+export { createDispatcher } from "./createDispatcher.ts";
+export { splitActionType } from "./splitActionTypes.ts";
 export * from "./types.ts";
